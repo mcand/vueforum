6f2f4c8b17a704fa563439b2f1cbb6260c24157e
Andre Furquim <afurquim@mcfadyen.com>
2022-02-16 15:07:31 -0300

    create base component

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -8,7 +8,7 @@
             <router-link :to="{name: 'Thread', params: {id: thread.id}}">{{ thread.title }}</router-link>
           </p>
           <p class="text-faded text-xsmall">
-            By <a href="#">{{ userById(thread.userId).name}}</a>, {{ thread.publishedAt}}.
+            By <a href="#">{{ userById(thread.userId).name}}</a>, <AppDate :timestamp="thread.publishedAt" />.
           </p>
         </div>
         <div class="activity">
@@ -22,7 +22,7 @@
             <p class="text-xsmall">
               <a href="#">{{ userById(thread.userId).name }}</a>
             </p>
-            <p class="text-xsmall text-faded">{{ thread.publshedAt }}</p>
+            <p class="text-xsmall text-faded"><AppDate :timestamp="thread.publishedAt" /></p>
           </div>
         </div>
       </div>
@@ -32,12 +32,16 @@
 
 <script>
 import sourceData from '@/data.json'
+import AppDate from '@/components/AppDate'
 export default {
   props: {
     threads: {
       type: Array,
       required: true
     }
+  },
+  components: {
+    AppDate
   },
   data () {
     return {
