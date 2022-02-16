bb3c3fd41d42cd69e7f0149b70a3f7d46323788d
Andre Furquim <afurquim@mcfadyen.com>
2022-02-16 15:34:39 -0300

    registering base components globally

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -32,16 +32,12 @@
 
 <script>
 import sourceData from '@/data.json'
-import AppDate from '@/components/AppDate'
 export default {
   props: {
     threads: {
       type: Array,
       required: true
     }
-  },
-  components: {
-    AppDate
   },
   data () {
     return {
