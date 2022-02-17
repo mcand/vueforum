9dbfe5f77c7eaf87c0db65a1ed5038f0103ee2ef
Andre Furquim <afurquim@mcfadyen.com>
2022-02-17 20:12:07 -0300

    fetching the authenticated user frrom vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
 import { createStore } from 'vuex'
-import sourceDate from '@/data'
+import sourceData from '@/data'
 
 export default createStore({
-  state: sourceDate,
+  state: {
+    ...sourceData,
+    authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
+  },
+  getters: {
+    authUser: state => state.users.find(user => user.id === state.authId)
+  },
   actions: {
     createPost (context, post) {
-      post.id = 'qqqq' + Math.random()
+      post.id = 'ggqq' + Math.random()
       context.commit('setPost', { post }) // set the post
       context.commit('appendPostToThread', { postId: post.id, threadId: post.threadId }) // append post to thread
     }
