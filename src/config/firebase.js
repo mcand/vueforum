123a9c3f13aa171e8766bd3ad8a94b3e05f86acd
Andre Furquim <andreccomputacao@gmail.com>
2022-04-05 14:01:31 -0300

    firebase setup

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyBMbq_x8diPg2irfaaQ92j7S3DStosfoYQ',
+  authDomain: 'vue-school-forum-f98ee.firebaseapp.com',
+  projectId: 'vue-school-forum-f98ee',
+  storageBucket: 'vue-school-forum-f98ee.appspot.com',
+  messagingSenderId: '960622863915',
+  appId: '1:960622863915:web:c0b286dca6c3f2c6c5ec6e'
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
+import firebase from 'firebase/app'
+import firebaseConfig from '@/config/firebase'
+
+firebase.initializeApp(firebaseConfig)
 
 const forumApp = createApp(App)
 forumApp.use(router)
